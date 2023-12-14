84f314b5b567813632464d729468aa475914e912
EmperorSARATH <ukm7773@gmail.com>
2023-12-14 23:41:06 +0530

    Router change removed

--- threejs/src/App.jsx
+++ threejs/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
@@ -8,21 +8,20 @@ import * as Scroll from 'react-scroll';
 function App() {
   const Element = Scroll.Element;
   return (
-    <BrowserRouter>
+   <div>
      <div className='relative z-0 bg-primary '>
      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-      <Routes>
       <Element name="hero">
-      <Route path="/" exact element={<Hero/>}/>
+          <Hero/>
+
       </Element>
-      <Route path="/" exact element={<About/>}/>
+        <About/>
         <br/> <br/> <br/> <br/> <br/> <br/><br/> 
-        <Route path="/" exact element={<Projects/>}/>
-      </Routes>
+        <Projects/>
         </div>
      </div>
 
-    </BrowserRouter>
+     </div>
   );
 }
 
